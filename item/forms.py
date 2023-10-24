a7acc869178f2060eb34add5f80faa3ca092ce2e
DidusOleh <didok1510@gmail.com>
2023-10-25 00:34:57 +0300

    first commit

---
+++ item/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from .models import Item
+
+INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border"
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ("category", "name", "description", "donation", "image")
+        widgets = {
+            "category": forms.Select(attrs={"class": INPUT_CLASSES}),
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "descrition": forms.Textarea(attrs={"class": INPUT_CLASSES}),
+            "donation": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
+        }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ("name", "description", "donation", "image", "is_over")
+        widgets = {
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "descrition": forms.Textarea(attrs={"class": INPUT_CLASSES}),
+            "donation": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
+        }
