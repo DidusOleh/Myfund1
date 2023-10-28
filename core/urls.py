4207e90ba05ccea63ac27760c29da9bb13173d94
unknown <notsemka@gmail.com>
2023-10-28 18:20:21 +0300

    test logout

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as authViews
 from django.urls import path
 from . import views
 from .forms import LoginForm
@@ -9,6 +10,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
+    path('exit/', authViews.LogoutView.as_view(template_name="core/logout.html"), name='logout'),
     path(
         "login/",
         auth_views.LoginView.as_view(
